3dd66951cf54c6675c3db07dd54f0ecb2decf446
nazia zareen <naziazareen59@gmail.com>
2025-05-18 10:28:32 +0530

    Add HelloWorld class and .gitignore for compiled files

--- hello.java
+++ hello.java
@@ -1,13 +1,13 @@
 
 import java.util.*;
-class hello
-{
-    public static void main(String args[])
-    {
-        Scanner sc= new Scanner(System.in);
+
+class hello {
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("enter the number");
-        int n =sc.nextInt();
-        int sq=n*n;
-        System.out.println("The sq is "+sq);
+        int n = sc.nextInt();
+        int sq = n * n;
+        System.out.println("The sq is " + sq);
+        sc.close();
     }
 }
